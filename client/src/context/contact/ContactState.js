9a4a2091019ad52e64acedb2c1389881a21dbe6e
GiuliaGallorini <giuliagallorini7@gmail.com>
2019-09-20 15:56:42 +0100

    Delete Contact from UI

--- client/src/context/contact/ContactState.js
+++ client/src/context/contact/ContactState.js
@@ -48,6 +48,9 @@ const ContactState = props => {
   };
 
   // Delete Contact
+  const deleteContact = id => {
+    dispatch({ type: DELETE_CONTACT, payload: id });
+  };
 
   // Set Current Contact
 
@@ -60,10 +63,12 @@ const ContactState = props => {
   // Clear Filter
 
   return (
+    // The provider will provide these functions to your component
     <ContactContext.Provider
       value={{
         contacts: state.contacts,
-        addContact
+        addContact,
+        deleteContact
       }}
     >
       {props.children}
