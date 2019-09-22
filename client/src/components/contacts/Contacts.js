57ecf4adfb89ac2f87f1ae7c9c620d86415663f5
GiuliaGallorini <giuliagallorini7@gmail.com>
2019-09-22 12:36:18 +0100

    created searchbar for contacts: ContactFilter component and FILTER_CONTACTS

--- client/src/components/contacts/Contacts.js
+++ client/src/components/contacts/Contacts.js
@@ -8,13 +8,21 @@ const Contacts = () => {
   // so you have access to any state or methods or actions associated with this context
   const contactContext = useContext(ContactContext);
 
-  const { contacts } = contactContext;
+  const { contacts, filtered } = contactContext;
+
+  if (contacts.length === 0) {
+    return <h4>Please add a contact</h4>;
+  }
 
   return (
     <Fragment>
-      {contacts.map(contact => (
-        <ContactItem key={contact.id} contact={contact} />
-      ))}
+      {filtered !== null
+        ? filtered.map(contact => (
+            <ContactItem key={contact.id} contact={contact} />
+          ))
+        : contacts.map(contact => (
+            <ContactItem key={contact.id} contact={contact} />
+          ))}
     </Fragment>
   );
 };
