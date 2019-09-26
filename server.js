e2599d7b76493ae6bbcd3d07f7fdbd1383254a53
GiuliaGallorini <giuliagallorini7@gmail.com>
2019-09-26 14:37:49 +0100

    Prepare for deploy

--- server.js
+++ server.js
@@ -5,6 +5,7 @@
 const express = require('express');
 
 const connectDB = require('./config/db');
+const path = require('path');
 
 // Initialise Express in a variable called app
 const app = express();
@@ -24,6 +25,16 @@ app.get('/', (req, res) =>
 app.use('/api/users', require('./routes/users'));
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/contacts', require('./routes/contacts'));
+
+// Serve static assets in production
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  );
+}
 
 // Create a variable for the PORT: first it will look for a environment variable called PORT, during the production, or any PORT you want, ex. 5000
 const PORT = process.env.PORT || 5000;
